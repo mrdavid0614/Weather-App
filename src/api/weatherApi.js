4c7c54148e15e86a6ec88be585ec5611e2570ccf
Raymond D. Mora <61632625+MrDavid0614@users.noreply.github.com>
2023-01-05 00:54:14 -0400

    Change the backend proxy url

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -1,19 +1,24 @@
-export const baseUrl = "https://raymond-cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location";
+export const baseUrl =
+  "https://raymond-cors-anywhere-production.up.railway.app/https://www.metaweather.com/api/location";
 
 export const getLocationWeather = async (locationId) => {
-    try {
-        const locationWeather = await (await (fetch(`${ baseUrl }/${ locationId }`))).json();
-        return locationWeather;
-    } catch (error) {
-        return {}
-    }
+  try {
+    const locationWeather = await (
+      await fetch(`${baseUrl}/${locationId}`)
+    ).json();
+    return locationWeather;
+  } catch (error) {
+    return {};
+  }
 };
 
 export const getLocationsWeatherByKeyword = async (keyword) => {
-    try {
-        const location = await (await fetch(`${ baseUrl }/search/?query=${ keyword }`)).json();
-        return location;
-    } catch (error) {
-        return [];
-    }
-}
+  try {
+    const location = await (
+      await fetch(`${baseUrl}/search/?query=${keyword}`)
+    ).json();
+    return location;
+  } catch (error) {
+    return [];
+  }
+};
